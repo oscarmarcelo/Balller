c5ee5cc0c2a76e4d9c687af700421d216159a560
Oscar Marcelo <oscarmarcelofg@hotmail.com>
2015-03-22 04:17:57 +0000

    All read-only endpoints added
    Added all the remaining read-only endpoints. Refactored and rename
    get(), which is now request(). Updated README.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A PHP wrapper for the [Dribbble API v1](http://developer.dribbble.com/v1/).
 ## How to use
  1. An application is now required. [Register](https://dribbble.com/account/applications/new) a new one.
  2. There are two ways to use the API.
-  - Using an OAuth authorization code, which gives full access to the API.
+  - Using an OAuth authorization code, which gives full access to the API (Coming soon).
   - Using the Client Access Token, which gives a read-only access to the API.
 
  3. Paste the following code. Replace with you token, and include file path, if needed:
@@ -17,18 +17,51 @@ $dribbble = new Dribbble($token);
 ```
  4. Using the wrapper methods is easy as:
 ```php
-$dribbble->get('user/shots', array('per_page' => 20));
+$dribbble->request('user/shots', array('per_page' => 20));
 ``` 
  5. Good Work! :)
 
 ##Methods
-Method | Description | API Documentation
--------|-------------|------------------
-`get($endpoint, $params = array())` | The main GET method used by other methods | *none*
-`user($user, $params = array())`|Gets information of a user|GET [/users/:user](http://developer.dribbble.com/v1/users/#get-a-single-user)
-`current_user($params = array())`|Gets information of the authenticated user|GET [/user](http://developer.dribbble.com/v1/users/#get-the-authenticated-user)
-`user_shots($user, $params = array())` | Gets shots of a user | GET [/users/:user/shots](http://developer.dribbble.com/v1/users/shots/)
-`current_user_shots($params = array())` | Gets the shots of the authenticated user | GET [/user/shots](http://developer.dribbble.com/v1/users/shots/)
+
+ - request($endpoint, $params = array())
+ - bucket($id, $params = array())
+ - bucket_shots($id, $params = array())
+ - project($id, $params = array())
+ - project_shots($id, $params = array())
+ - shots($list = false, $timeframe = false, $date = false, $sort = false, $params = array())
+ - shot($id)
+ - shot_attachments($id, $params = array())
+ - attachment($shot_id, $attachment_id)
+ - shot_buckets($id)
+ - shot_comments($id, $params = array())
+ - comment_likes($shot_id, $comment_id, $params = array())
+ - comment($shot_id, $comment_id)
+ - has_liked_comment($shot_id, $comment_id)
+ - shot_likes($id, $params = array())
+ - has_liked_shot($id)
+ - shot_projects($id, $params = array())
+ - shot_rebounds($id, $params = array())
+ - team_members($team, $params = array())
+ - team_shots($team, $params = array())
+ - user($user, $params = array())
+ - current_user($params = array())
+ - user_buckets($user, $params = array())
+ - current_user_buckets($params = array())
+ - user_followers($user, $params = array())
+ - current_user_followers($params = array())
+ - user_following($user, $params = array())
+ - current_user_following($params = array())
+ - current_user_following_shots($params = array())
+ - is_current_user_following($user)
+ - is_user_following($user, $target_user)
+ - user_likes($user, $params = array())
+ - current_user_likes($params = array())
+ - user_projects($user, $params = array())
+ - current_user_projects($params = array())
+ - user_shots($user, $params = array())
+ - current_user_shots($params = array())
+ - user_teams($user, $params = array())
+ - current_user_teams($params = array())
 
 ##Response
 The wrapper return the responses as PHP objects.  
@@ -36,5 +69,4 @@ If any errors are encountered, then a `Exception` is thrown, which can be caught
 
 ##To do
 
- - Finish methods on Lite version
  - Do a full version (with authorization) of the wrapper
--- src/Dribbble-lite.php
+++ src/Dribbble-lite.php
@@ -25,8 +25,8 @@ class Dribbble {
 	);
 
 	/**
-	 * The Class constructer
-	 * @param type array $settings The settings to be used.
+	 * The Class constructor
+	 * @param type array $token The Client Access Token to be used.
 	 * @return type
 	 */
 	function __construct($token) {
@@ -47,7 +47,7 @@ class Dribbble {
 	 * @param array $params The parameters for the endpoint.
 	 * @return object
 	 */
-	function get($endpoint, $params = array()) {
+	private function request($endpoint, $params = array()) {
 
 		$ch = curl_init();
 		$options = $this->curl_options;
@@ -60,17 +60,152 @@ class Dribbble {
 		$result = curl_exec($ch);
 		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		curl_close($ch);
+		if ($error = curl_error($ch))
+			throw new Exception($error, curl_errno($ch));
 
-		if ($result === false)
-			throw new Exception(curl_error($ch), curl_errno($ch));
+		curl_close($ch);
 
 		$verify = json_decode($result);
 
 		if (isset($verify->message))
 			throw new Exception($verify->message, $status);
 
-		return $result;
+		return array(
+			'status' => $status,
+			'result' => $result
+			);
+
+	}
+
+	function bucket($id, $params = array()) {
+
+		return $this->request(sprintf('/buckets/%d', $id), $params)['result'];
+
+	}
+
+	function bucket_shots($id, $params = array()) {
+
+		return $this->request(sprintf('/buckets/%d/shots', $id), $params)['result'];
+
+	}
+
+	function project($id, $params = array()) {
+
+		return $this->request(sprintf('/projects/%d', $id), $params)['result'];
+
+	}
+
+	function project_shots($id, $params = array()) {
+
+		return $this->request(sprintf('/projects/%d/shots', $id), $params)['result'];
+
+	}
+
+	function shots($list = false, $timeframe = false, $date = false, $sort = false, $params = array()) {
+
+		if ($list)      $params['list'] =      $list;
+		if ($timeframe) $params['timeframe'] = $timeframe;
+		if ($date)      $params['date'] =      date('Y-m-d', strtotime($date));
+		if ($sort)      $params['sort'] =      $sort;
+
+		return $this->request('/shots', $params)['result'];
+
+	}
+
+	function shot($id) {
+
+		return $this->request(sprintf('/shots/%d', $id), $params)['result'];
+
+	}
+
+	function shot_attachments($id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%d/attachments', $id), $params)['result'];
+
+	}
+
+	function attachment($shot_id, $attachment_id) {
+
+		return $this->request(sprintf('/shots/%1$d/attachments/%2$d', $shot_id, $attachment_id), $params)['result'];
+
+	}
+
+	function shot_buckets($id) {
+
+		return $this->request(sprintf('/shots/%d/buckets', $id), $params)['result'];
+
+	}
+
+	function shot_comments($id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%d/comments', $id), $params)['result'];
+
+	}
+
+	function comment_likes($shot_id, $comment_id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%1$d/comments/%2$d/likes', $shot_id, $comment_id), $params)['result'];
+
+	}
+
+	function comment($shot_id, $comment_id) {
+
+		return $this->request(sprintf('/shots/%1$d/comments/%2$d', $shot_id, $comment_id), $params)['result'];
+
+	}
+
+	function has_liked_comment($shot_id, $comment_id) {
+
+		$response = $this->request(sprintf('/shots/%1$d/comments/%2$d/like', $shot_id, $comment_id), $params);
+
+		if ($response['status'] == '200') {
+			return $response['result'];
+		} elseif ($response['status'] == '404') {
+			return false;
+		}
+
+	}
+
+
+	function shot_likes($id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%d/likes', $id), $params)['result'];
+
+	}
+
+	function has_liked_shot($id) {
+
+		$response = $this->request(sprintf('/shots/%d/like', $id), $params);
+
+		if ($response['status'] == '200') {
+			return $response['result'];
+		} elseif ($response['status'] == '404') {
+			return false;
+		}
+
+	}
+
+	function shot_projects($id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%d/projects', $id), $params)['result'];
+
+	}
+
+	function shot_rebounds($id, $params = array()) {
+
+		return $this->request(sprintf('/shots/%d/rebounds', $id), $params)['result'];
+
+	}
+
+	function team_members($team, $params = array()) {
+
+		return $this->request(sprintf('/teams/%s/members', $team), $params)['result'];
+
+	}
+
+	function team_shots($team, $params = array()) {
+
+		return $this->request(sprintf('/teams/%s/shots', $team), $params)['result'];
 
 	}
 
@@ -82,7 +217,7 @@ class Dribbble {
 	 */
 	function user($user, $params = array()) {
 
-		return $this->get(sprintf('/users/%s', $user), $params);
+		return $this->request(sprintf('/users/%s', $user), $params)['result'];
 
 	}
 
@@ -93,7 +228,130 @@ class Dribbble {
 	 */
 	function current_user($params = array()) {
 
-		return $this->get('/user', $params);
+		return $this->request('/user', $params)['result'];
+
+	}
+
+	/**
+	 * Gets the buckets of a user
+	 * @param mixed $user The username or id of a user
+	 * @param array $params 
+	 * @return object
+	 */
+	function user_buckets($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/buckets', $user), $params)['result'];
+
+	}
+
+	/**
+	 * Gets the buckets of the authenticated user
+	 * @param array $params 
+	 * @return object
+	 */
+	function current_user_buckets($params = array()) {
+
+		return $this->request('/user/buckets', $params)['result'];
+
+	}
+
+	function user_followers($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/followers', $user), $params)['result'];
+
+	}
+
+	function current_user_followers($params = array()) {
+
+		return $this->request('/user/followers', $params)['result'];
+
+	}
+
+	function user_following($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/following', $user), $params)['result'];
+
+	}
+
+	function current_user_following($params = array()) {
+
+		return $this->request('/user/following', $params)['result'];
+
+	}
+
+	function current_user_following_shots($params = array()) {
+
+		return $this->request('/user/following/shots', $params)['result'];
+
+	}
+
+	function is_current_user_following($user) {
+
+		$status = $this->request(sprintf('/user/following/%s', $user), $params)['status'];
+
+		if ($status == '204') {
+			return true;
+		} elseif($status == '404') {
+			return false;
+		}
+
+	}
+
+	function is_user_following($user, $target_user) {
+
+		$status = $this->request(sprintf('/users/%1$s/following/%2$s', $user, $target_user), $params)['status'];
+
+		if ($status == '204') {
+			return true;
+		} elseif($status == '404') {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Gets the likes of a user
+	 * @param mixed $user The username or id of a user
+	 * @param array $params 
+	 * @return object
+	 */
+	function user_likes($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/likes', $user), $params)['result'];
+
+	}
+
+	/**
+	 * Gets the likes of the authenticated user
+	 * @param array $params 
+	 * @return object
+	 */
+	function current_user_likes($params = array()) {
+
+		return $this->request('/user/likes', $params)['result'];
+
+	}
+
+	/**
+	 * Gets the projects of a user
+	 * @param mixed $user The username or id of a user
+	 * @param array $params 
+	 * @return object
+	 */
+	function user_projects($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/projects', $user), $params)['result'];
+
+	}
+
+	/**
+	 * Gets the projects of the authenticated user
+	 * @param array $params 
+	 * @return object
+	 */
+	function current_user_projects($params = array()) {
+
+		return $this->request('/user/projects', $params)['result'];
 
 	}
 
@@ -105,7 +363,7 @@ class Dribbble {
 	 */
 	function user_shots($user, $params = array()) {
 
-		return $this->get(sprintf('/users/%s/shots', $user), $params);
+		return $this->request(sprintf('/users/%s/shots', $user), $params)['result'];
 
 	}
 
@@ -114,9 +372,32 @@ class Dribbble {
 	 * @param array $params 
 	 * @return object
 	 */
-	function current_user_shots(, $params = array()) {
+	function current_user_shots($params = array()) {
 
-		return $this->get('/user/shots', $params);
+		return $this->request('/user/shots', $params)['result'];
+
+	}
+
+	/**
+	 * Gets the teams of a user
+	 * @param mixed $user The username or id of a user
+	 * @param array $params 
+	 * @return object
+	 */
+	function user_teams($user, $params = array()) {
+
+		return $this->request(sprintf('/users/%s/teams', $user), $params)['result'];
+
+	}
+
+	/**
+	 * Gets the teams of the authenticated user
+	 * @param array $params 
+	 * @return object
+	 */
+	function current_user_teams($params = array()) {
+
+		return $this->request('/user/teams', $params)['result'];
 
 	}
 
